7265177bedb490cc7f10f1d6dfe60335a45ca527
Ruben Berenguel <ruben@mostlymaths.net>
2020-05-28 23:02:03 +0200

    Made the GUI draggable, changed hide for collapse

--- libraries/gui/dom.js
+++ libraries/gui/dom.js
@@ -5,5 +5,6 @@ export { $ }
 let $ = {
     cel: (s) => document.createElement(s),
     ctn: (s) => document.createTextNode(s),
-    byId: (s) => document.getElementById(s)
+    byId: (s) => document.getElementById(s),
+    qs: (s) => document.querySelector(s),
 }
