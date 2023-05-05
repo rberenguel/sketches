bc6603746d33605c453288beb2ede6ae1ced7730
Ruben Berenguel <ruben@mostlymaths.net>
2023-05-06 00:04:39 +0200

    Improving painting

--- 2/2.js
+++ 2/2.js
@@ -1,25 +1,53 @@
 import {
+  createBaseGUI,
   Command,
   GUI,
   Integer,
   Float,
   Key,
-  Control
+  Control,
+  Seeder
 } from '../libraries/gui/gui.js'
 
 import {
-  getLargeCanvas, copyColor, darken
+  createBlots,
+  drawCloud,
+  drawBlot,
+  canvasUpdate,
+  blotPtsInMesh,
+  paintPalette
+} from '../libraries/blotLibraries.js'
+
+import {
+  getLargeCanvas,
+  signature,
+  copyColor,
+  darken
 } from '../libraries/misc.js'
 
-
-// Base to avoid writing always the same
 
 const sketch = (s) => {
 
   let gui
-  let largeCanvas
-  let hd = 1
+  let seed = 42
+  let debug = true
+  let dly // Base debug layer, if used
+
+  // Globals needed in controls, commands or deep arguments
+  let cfg = {
+    hd: 1,
+    seeder: undefined,
+    largeCanvas: undefined
+  }
+
+  let W, H // Helpful globals to avoid typing scene.width so much
+
   const PI = s.PI
+
+  s.preload = () => {
+    cfg.font = s.loadFont("../libraries/fonts/Monoid-Retina.ttf")
+  }
+
   s.setup = () => {
     let {
       w,
@@ -30,188 +58,310 @@ const sketch = (s) => {
     canvas.mousePressed(() => {})
     s.frameRate(20)
     s.noLoop()
+    cfg.seeder = new Seeder()
     gui = createGUI()
     gui.toggle()
   }
-  // From Piter Pasma
-  const smoothStep = (a,b,x)=>(x-=a,x/=b-a,x<0?0:x>1?1:x*x*(3-2*x));
 
-  function wobble(scene, cx, cy, r, c, seed){
-	const d = copyColor(s, c)
-	const da = s.alpha(d)
-	if(seed){
-		scene.randomSeed(seed)
-		scene.noiseSeed(seed)	
-	}
-    const wx = cx + (2*r-scene.random(r)) << 0
-    const wy = cy + (2*r-scene.random(r)) << 0    
-	const maxJ = 3
-	for(let j=0;j<maxJ;j++){
-		const smj = smoothStep(0, maxJ, j)
-		const rr = (5-5*smj)*r+0.5*scene.noise(cx, cy)*r
-		d.setAlpha(da*smj)
-		scene.fill(d)
-		scene.stroke(d)
-		scene.beginShape()
-    	for(let i=0;i<=15;i++){
-      		const w = 0.2*rr+0.8*rr*scene.noise(cx, cy, i)
-      		const x = wx + w*Math.cos(i*2*PI/15)
-      		const y = wy + 2*w*Math.sin(i*2*PI/15)      
-      		scene.vertex(x, y)
-    	}
-    	scene.endShape(s.CLOSE)
-	}
+  const smoothStep = (a, b, x) => (x -= a, x /= b - a, x < 0 ? 0 : x > 1 ? 1 : x * x * (3 - 2 * x));
+
+  function wobble(scene, cx, cy, r, c, seed) {
+    const d = copyColor(s, c)
+    const da = s.alpha(d)
+    if (seed) {
+      scene.randomSeed(seed)
+      scene.noiseSeed(seed)
+    }
+    const wx = cx + (2 * r - scene.random(r)) << 0
+    const wy = cy + (2 * r - scene.random(r)) << 0
+    const maxJ = 3
+    for (let j = 0; j < maxJ; j++) {
+      const smj = smoothStep(0, maxJ, j)
+      const rr = (5 - 5 * smj) * r + 0.5 * scene.noise(cx, cy) * r
+      d.setAlpha(da * smj)
+      scene.fill(d)
+      scene.stroke(d)
+      scene.beginShape()
+      for (let i = 0; i <= 15; i++) {
+        const w = 0.2 * rr + 0.8 * rr * scene.noise(cx, cy, i)
+        const x = wx + w * Math.cos(i * 2 * PI / 15)
+        const y = wy + 2 * w * Math.sin(i * 2 * PI / 15)
+        scene.vertex(x, y)
+      }
+      scene.endShape(s.CLOSE)
+    }
   }
-  
-  
-  function updown(scene, x, y, width, length, color){
+
+  // Suggestion (Jeff Palmer) vary the color per bristle!
+
+  function downStroke(scene, x, y, width, length, color) {
+    scene.push()
     let load = []
-    let previousLoad = []    
+    let previousLoad = []
     let maxCap = 0
-    for(let i=0;i<width;i+=2){
-      load[i] = 1*length*Math.sqrt(scene.noise(i/8))*scene.noise(i/16)
-      if(load[i]>maxCap){
+    for (let i = 0; i < width; i += 2) {
+      load[i] = 1 * length * Math.sqrt(scene.noise(i / 8)) * scene.noise(i / 16)
+      if (load[i] > maxCap) {
         maxCap = load[i]
       }
       previousLoad[i] = 1
     }
-    console.log(length, maxCap)
     let c = copyColor(s, color)
     let d = darken(s, c, 0.95)
-    for(let i=0; i< length; i+=1){
-      for(let j=0;j<width;j+=2){
-        if(i<length/8.0){
-          if(scene.noise(j/8) > smoothStep(0, length/8, i)){
+    for (let i = 0; i < length; i += 1) {
+      for (let j = 0; j < width; j += 2) {
+        if (i < length / 8.0) {
+          if (scene.noise(j / 8) > smoothStep(0, length / 8, i)) {
             continue
           }
         }
         const pigment = load[j]
-        const op = smoothStep(0, maxCap, maxCap-pigment)
-        const nop = 0.5*smoothStep(0, 0.7*length, i)
+        const op = smoothStep(0, maxCap, maxCap - pigment)
+        const nop = 0.5 * smoothStep(0, 0.7 * length, i)
         const previous = previousLoad[j]
-//        const drop = previous*scene.noise(i/50)+2*scene.random()
-        const drop = Math.abs(0.5*previous+0.5*previous*scene.noise(i/50)+(-2+4*scene.random()))
-        if(pigment > drop){
-          load[j]-=drop
+        const drop = Math.abs(0.5 * previous + 0.5 * previous * scene.noise(i / 50) + (-2 + 4 * scene.random()))
+        if (pigment > drop) {
+          load[j] -= drop
           previousLoad[j] = drop
-          c.setAlpha(255)  
+          c.setAlpha(255)
         } else {
-          if(previous<0.01){
+          if (previous < 0.01) {
             let e = copyColor(s, c)
-            e.setAlpha(140*(0.5-nop))
+            e.setAlpha(140 * (0.5 - nop))
+            if(i>0.7*length){
+              e.setAlpha(0)
+            }
             scene.stroke(e)
             scene.fill(e)
-            wobble(scene, x+j, y+i, 0.4*drop, e)
+            wobble(scene, x + j, y + i, 0.4 * drop, e)
           }
           continue
         }
-        if(drop<1){
-          //c.setAlpha(255*drop*drop)
-        } else {
-          //c.setAlpha(255)
+        if (scene.random() < 0.2) {
+          continue
         }
-        if(scene.random()<0.2){continue}//{c.setAlpha(255-op*255)}
-        scene.fill(c)
+        //scene.fill(c)
+        let b
+        if(pigment < 1){
+          b = darken(s, c, 1.5*pigment)
+        } else {
+          b = c
+        }
         scene.strokeWeight(0.5)
-		const internalSeed = 10000*scene.random() << 0
-        wobble(scene, x+j, y+i, 0.35*drop, d, internalSeed)
-        //scene.stroke(c)
-        wobble(scene, x+j, y+i, 0.3*drop, c, internalSeed)
+        const internalSeed = 10000 * scene.random() << 0
+        wobble(scene, x + j, y + i, 0.35 * drop, d, internalSeed)
+        wobble(scene, x + j, y + i, 0.3 * drop, b, internalSeed)
       }
     }
+    scene.pop()
   }
-  
-  
+
+  function sideStroke(scene, x, y, width, length, color) {
+    scene.push()
+    let load = []
+    let previousLoad = []
+    let maxCap = 0
+    for (let i = 0; i < width; i += 2) {
+      load[i] = 1 * length * Math.sqrt(scene.noise(i / 8)) * scene.noise(i / 16)
+      if (load[i] > maxCap) {
+        maxCap = load[i]
+      }
+      previousLoad[i] = 0.6
+    }
+    let c = copyColor(s, color)
+    let d = darken(s, c, 0.95)
+    for (let j = 0; j < width; j += 2) {
+      for (let i = 0; i < length; i += 1) {
+        if (i < length / 8.0) {
+          if (scene.noise(j / 8) > smoothStep(0, length / 8, i)) {
+            continue
+          }
+        }
+        const pigment = load[j]
+        const op = smoothStep(0, maxCap, maxCap - pigment)
+        const nop = 0.5 * smoothStep(0, length / 2, i) // This creates the smooth drop
+        const previous = previousLoad[j]
+        const drop = Math.abs(0.5 * previous + 0.5 * previous * scene.noise(i / 50) + (-2 + 4 * scene.random()))
+        if (pigment > drop) {
+          load[j] -= drop
+          previousLoad[j] = drop
+          c.setAlpha(255)
+        } else {
+          if (previous < 0.1) {
+            let e = copyColor(s, c)
+            e.setAlpha(140 * (0.5 - nop))
+            if(i>0.7*length){
+              e.setAlpha(0)
+            }
+            scene.stroke(e)
+            scene.fill(e)
+            wobble(scene, x + i, y + j, 0.4 * drop, e)
+          }
+          continue
+        }
+        if (scene.random() < 0.6) {
+          continue
+        }
+        if (pigment < 1) {
+          load[j] = 0
+        }
+        scene.fill(c)
+        scene.strokeWeight(0.5)
+        const internalSeed = 10000 * scene.random() << 0
+        wobble(scene, x + i, y + j, 0.35 * drop, d, internalSeed)
+        wobble(scene, x + i, y + j, 0.3 * drop, c, internalSeed)
+        if (j >= 0.9 * width) {
+          if (drop > 1 && scene.random() < 0.1) {
+            const drip = 30 * drop
+            for (let k = 0; k < drip; k++) {
+              let fop = 0.5 * smoothStep(0, 0.9 * drip, k)
+              let e = copyColor(s, c)
+              e.setAlpha(200 * (0.5 - fop))
+              scene.stroke(e)
+              scene.fill(e)
+              wobble(scene, x + i, y + j + k, 0.1 * drop, e)
+            }
+          }
+        }
+      }
+    }
+    scene.pop()
+  }
+
+
   // This generates an interesting "at sea" effect, unexpectedly
-  function interestingWave(scene, x, y, width, length){
+  function interestingWave(scene, x, y, width, length) {
     scene.fill("black")
     scene.stroke("black")
     scene.strokeWeight(1)
     let c = s.color(0, 0, 50, 255)
-    for(let i=0; i< length; i+=4){
+    for (let i = 0; i < length; i += 4) {
       const op = smoothStep(0, length, i)
       //console.log(op)
-      c.setAlpha(255-200*op)
+      c.setAlpha(255 - 200 * op)
       scene.fill(c)
       scene.stroke(c)
       //scene.line(x, y+i, x+width, y+i)
-      for(let j=0;j<width;j+=3){
-        const di = 4-(scene.noise(i/(100*op), j/(100*op))*2*8)
-        scene.circle(x+j, y+i+di, 8*scene.noise(i/(100*op), j/(100*op)))
+      for (let j = 0; j < width; j += 3) {
+        const di = 4 - (scene.noise(i / (100 * op), j / (100 * op)) * 2 * 8)
+        scene.circle(x + j, y + i + di, 8 * scene.noise(i / (100 * op), j / (100 * op)))
       }
     }
   }
-  
-  
+
+  function baseEvenMesh(spread) {
+    let pxs = []
+    for (let j = 0; j < spread; j++) {
+      for (let i = 0; i < spread; i++) {
+        let cx = spread / 2.0 - i
+        let cy = spread / 2.0 - j
+        if (Math.sqrt(cx * cx + cy * cy) > 200) continue
+        pxs.push([i, j])
+      }
+    }
+    return pxs
+  }
+
   s.draw = () => {
-    const numPixels = hd * s.width * hd * s.height
-    let scene = s.createGraphics(hd * s.width, hd * s.height)
-    // Here your code against scene
-    scene.background("#999960")
-    const w1 = scene.random(0.05*scene.width, 0.1*scene.width)
-    const x1 = 0.4*scene.width//scene.random(0.1*scene.width, 0.3*scene.width)
-    const y1 = 0.1*scene.height//scene.random(0.2*scene.height, 0.4*scene.height)
-    const l1 = scene.random(0.6*scene.height, 0.6*scene.height)
+    let scene = s.createGraphics(cfg.hd * s.width, cfg.hd * s.height)
+    W = scene.width, H = scene.height
+    let dly = s.createGraphics(W, H)
+    scene.randomSeed(cfg.seeder.get())
+    scene.noiseSeed(cfg.seeder.get())
+    dly.randomSeed(cfg.seeder.get())
+    dly.noiseSeed(cfg.seeder.get())
+
+    // Here your code against scene and possibly dly
+
+    scene.background("#353530")
+    const w1 = scene.random(0.02 * scene.width, 0.08 * scene.width)
+    const x1 = 0.12 * scene.width //scene.random(0.1*scene.width, 0.3*scene.width)
+    const y1 = 0.08 * scene.height //scene.random(0.2*scene.height, 0.4*scene.height)
+    const l1 = scene.random(0.6 * scene.height, 0.6 * scene.height)
     const black = s.color(10, 10, 10, 240)
     const white = s.color(240, 240, 240, 240)
     const red = s.color(200, 10, 10, 240)
-    updown(scene,  x1, y1, w1, 1.5*l1, red)
-    scene.noiseSeed(window.performance.now())
-    updown(scene,  x1+50, y1, w1, 1.5*l1, white)    
-    scene.noiseSeed(window.performance.now()+1000*scene.random())
-    updown(scene,  x1+100, y1, w1, 1.5*l1, black)    
-	
-    largeCanvas = scene
+    const blood = s.color("#880808")
+    let x = x1
+    let y = y1
+    downStroke(scene, x, y, w1, 1.5 * l1, black)
+    downStroke(scene, x + 0.2 * w1, y + w1, 0.5 * w1, 0.8 * l1, white)
+    sideStroke(scene, x + 0.8 * w1, y + 0.5 * w1, w1, 1.5 * l1, black)
+    sideStroke(scene, x + w1, y + 0.8 * w1, 0.5 * w1, 1.5 * l1, white)
+    x = x + 0.1 * scene.width
+    y = y + 0.1 * scene.height
+    downStroke(scene, x, y, w1, 1.5 * l1, black)
+    downStroke(scene, x + 0.2 * w1, y + w1, 0.5 * w1, 0.8 * l1, white)
+    sideStroke(scene, x + 0.8 * w1, y + 0.5 * w1, w1, 1.5 * l1, black)
+    sideStroke(scene, x + w1, y + 0.8 * w1, 0.5 * w1, 1.5 * l1, white)
+    x = 0.30 * scene.width
+    y = 0.30 * scene.height
+    const g1 = s.color("#00b25d")
+    const g2 = s.color("#0df24599")
+    downStroke(scene, x, y, 1.5 * w1, l1, g1)
+    downStroke(scene, x + 0.2 * w1, y + 1 * w1, 0.8 * w1, 0.8 * l1, g2)
+    x = 0.9 * scene.width
+    y = 0.05 * scene.height
+    downStroke(scene, x, y, 1.5 * w1, 2.5 * l1, black)
+    downStroke(scene, x + 0.2 * w1, y + 1.5 * w1, 0.8 * w1, 0.8 * l1, white)
+    
+    drawBlot({
+      s: s,
+      canvas: scene,
+      background: scene,
+      paint: 0,
+      drawPotential: false,
+    }, scene.width / 2, scene.height / 2, blood, {
+      mesh: baseEvenMesh(400),
+      blotPointsArray: new Array(5000)
+    }, {
+      blotCount: 20,
+      blotStrength: 200,
+      blotSpread: 50,
+      vectors: false,
+    })
+    
+    if (debug && dly) {
+      let c = dly.get()
+      scene.image(dly, 0, 0)
+    }
+
+    const identifier = `${cfg.seeder.get()}@${cfg.hd.toPrecision(2)}`
+    const sigCfg = {
+      s: s,
+      scene: scene,
+      color: "#101020",
+      shadow: "darkgrey",
+      fontsize: 9,
+      right: scene.width,
+      bottom: scene.height,
+      identifier: "", //identifier,
+      sig: "rb'23",
+      hd: cfg.hd,
+      font: cfg.font
+    }
+    signature(sigCfg)
+
+    cfg.largeCanvas = scene
     let c = scene.get()
     c.resize(s.width, 0)
     s.image(c, 0, 0)
   }
 
-  function createGUI() {
-    let info =
-      "Info"
-    let subinfo = "Subinfo<br/>Very high resolutions can fail depending on the browser"
-    let S = new Key("s", () => {
-      largeCanvas.save("img.png")
-    })
-    let saveCmd = new Command(S, "save the canvas")
-    let R = new Key("r", () => {
-      gui.spin(() => {
-        s.clear();
-        s.draw()
-        gui.spin();
-      });
-    });
 
-    let resetCanvas = new Command(R, "reset")
+  const createGUI = (gui) => {
+    cfg.title = "Something, RB 2020/"
+    cfg.info = "Info"
+    cfg.subinfo = "Subinfo<br/>Very high resolutions can fail depending on the browser"
+    cfg.s = s
+    cfg.commands = [cfg.seeder.command]
+    cfg.controls = [cfg.seeder.control]
 
-    let decH = new Key(",", () => {
-      if (hd > 0) {
-        hd -= 0.1
-      }
-    })
-    let incH = new Key(".", () => {
-      if (hd < 10) {
-        hd += 0.1
-      }
-    })
-    let hdInfo = new Float(() => hd)
-    let hdControl = new Control([decH, incH],
-      "+/- resolution export factor", hdInfo)
-
-
-    let gui = new GUI("Something, RB 2020/", info, subinfo, [saveCmd,
-        resetCanvas
-      ],
-      [hdControl])
-
-    let QM = new Key("?", () => gui.toggle())
-    let hide = new Command(QM, "hide this")
-
-    gui.addCmd(hide)
-    gui.update()
+    gui = createBaseGUI(cfg)
     return gui
   }
+
+
 
   s.keyReleased = () => {
     gui.dispatch(s.key)
